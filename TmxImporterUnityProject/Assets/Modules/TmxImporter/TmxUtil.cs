9637c8c96c4421090f31719f3f62d6abb43781fc
Myonghwa, Jung <kino811@gmail.com>
2016-09-07 23:26:52 +0900

    Now, Tsx-TileSet is Asset-File.

---
+++ TmxImporterUnityProject/Assets/Modules/TmxImporter/TmxUtil.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+namespace Kino.Tmx {
+    public class TmxUtil {
+        public static void CreateDirectory(string dirPath) {
+            if (!System.IO.Directory.Exists(dirPath)) {
+                System.IO.Directory.CreateDirectory(dirPath);
+            }
+        }
+    }
+}
